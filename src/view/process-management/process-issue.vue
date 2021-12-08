fa493519552f904a5bb2ee578c00eac726ae2277
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-12-08 23:33:01 +0800

    update: process management.

--- src/view/process-management/process-issue.vue
+++ src/view/process-management/process-issue.vue
@@ -1,36 +1,13 @@
 <template>
   <Card>
-    <Row type="flex" justify="end">
-      <Button
-        class="add-edge"
-        type="primary"
-        icon="md-add"
-        ghost
-        to="/edge_management/create-edge"
-      >
-      </Button>
-    </Row>
-    <Row>
-      <paged-table
-        :loading="loading"
-        :columns="columns"
-        :data-source="edgeList"
-        :total="edgeList.length"
-        style="margin-bottom: 50px"
-      />
-    </Row>
+
   </Card>
 </template>
 
 <script>
-import PagedTable from '_c/paged-table/paged-table.vue'
-import PopConfirmButton from '_c/pop-confirm-button'
-import { mapState, mapActions, mapMutations } from 'vuex'
-import { timeUnitNames } from '@/constants/timeUnits'
-import { edgeStatusNames } from '@/constants/edgeStatus'
 
 export default {
-  name: 'process-configuration',
+  name: 'process-issue',
   components: {
   },
   data: function () {
@@ -38,7 +15,7 @@ export default {
     }
   },
   methods: {
-    
+
   },
   mounted () {
 
