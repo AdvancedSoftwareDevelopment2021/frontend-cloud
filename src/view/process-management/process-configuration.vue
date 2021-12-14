a29b2a84f296565fb041928b885ad3e1279b64b3
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-12-14 23:07:56 +0800

    update: process configuration.

--- src/view/process-management/process-configuration.vue
+++ src/view/process-management/process-configuration.vue
@@ -1,14 +1,20 @@
 <template>
   <Card>
-
+    <Row>
+      <paged-table
+      />
+    </Row>
   </Card>
 </template>
 
 <script>
+import PagedTable from '_c/paged-table/paged-table.vue'
+import PagedTable from '../../components/paged-table/paged-table.vue'
 
 export default {
   name: 'process-configuration',
   components: {
+    PagedTable
   },
   data: function () {
     return {
