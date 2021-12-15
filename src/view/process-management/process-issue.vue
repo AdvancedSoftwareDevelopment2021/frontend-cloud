f8ef432c84e4584bb8f9b1619456e1685fb9219c
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-12-15 23:46:45 +0800

    update: update process-issue.

--- src/view/process-management/process-issue.vue
+++ src/view/process-management/process-issue.vue
@@ -1,10 +1,15 @@
 <template>
   <Card>
-
+    <Row>
+      <paged-table
+      />
+    </Row>
   </Card>
 </template>
 
 <script>
+import PagedTable from '_c/paged-table/paged-table.vue'
+import PagedTable from '../../components/paged-table/paged-table.vue'
 
 export default {
   name: 'process-issue',
